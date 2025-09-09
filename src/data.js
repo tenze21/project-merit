a901c24d31bc0125d127852908aa9c55bf3530de
tenze21 <tenzinchoda365@gmail.com>
2025-09-09 20:21:59 +0600

    fix data

--- src/data.js
+++ src/data.js
@@ -1,10 +1,9 @@
 export const students=[
-    {id: 12230054, name: "Tenzin Choda", address: "0x5Be35359710101eedb5F28caEa9b5198D40b5a10", stella: 0, tonitrus: 0, effectiveStella: 0},
-    {id: 12230045, name: "Tenzin Choda", address: "0xeAc0974a7848DB998244944b8E67E9a866dEc553", stella: 0, tonitrus: 0,  effectiveStella: 0},
-    {id: 1223004, name: "Tenzin Choda", address: "0x5Be35359710101eedb5F28caEa9b5198D40b5a10", stella: 0, tonitrus: 0,  effectiveStella: 0},
-    {id: 1223004, name: "Tenzin Choda", address: "0x5Be35359710101eedb5F28caEa9b5198D40b5a10", stella: 0, tonitrus: 0,  effectiveStella: 0},
-    {id: 1223004, name: "Tenzin Choda", address: "0x5Be35359710101eedb5F28caEa9b5198D40b5a10", stella: 0, tonitrus: 0,  effectiveStella: 0},
-    {id: 1223004, name: "Tenzin Choda", address: "0x5Be35359710101eedb5F28caEa9b5198D40b5a10", stella: 0, tonitrus: 0,  effectiveStella: 0},
-    {id: 1223004, name: "Tenzin Choda", address: "0x5Be35359710101eedb5F28caEa9b5198D40b5a10", stella: 0, tonitrus: 0,  effectiveStella: 0},
-    {id: 1223004, name: "Tenzin Choda", address: "0x5Be35359710101eedb5F28caEa9b5198D40b5a10", stella: 0, tonitrus: 0,  effectiveStella: 0},
+    {id: 12230055, name: "Tenzin Choda-1", address: "0xeAc0974a7848DB998244944b8E67E9a866dEc553", stella: 0, tonitrus: 0,  effectiveStella: 0},
+    {id: 12230056, name: "Tenzin Choda-2", address: "0x2193870DfFbd97c4E86F8791D6b3419b9D9dFf99", stella: 0, tonitrus: 0,  effectiveStella: 0},
+    {id: 12230057, name: "Tenzin Choda-3", address: "0x8B79E1518705df7f0f6095ADE86227f93734504C", stella: 0, tonitrus: 0,  effectiveStella: 0},
+    {id: 12230058, name: "Tenzin Choda-4", address: "0x0f47A19e94670061cec2d21F9d80954B3F7047E8", stella: 0, tonitrus: 0,  effectiveStella: 0},
+    {id: 12230059, name: "Tenzin Choda-5", address: "0x662396b1530842983cdF7EF168B1e137aEFDf06f", stella: 0, tonitrus: 0,  effectiveStella: 0},
+    {id: 12230060, name: "Tenzin Choda-6", address: "0x2B349AAEb45A4b10137341C981Cc52aBd7274dF4", stella: 0, tonitrus: 0,  effectiveStella: 0},
+    {id: 12230061, name: "Tenzin Choda-7", address: "0x771f5A17348cdC8B58377c43d199e471849fB431", stella: 0, tonitrus: 0,  effectiveStella: 0},
 ];
